77a35cc98eec726c5c76a333c632c7615e1a25a0
Alex <alex3rq@gmail.com>
2020-11-16 21:01:17 -0600

    fix: Error "undefined" dependency of CFG_ENABLED

--- MantisBTModernDarkTheme.php
+++ MantisBTModernDarkTheme.php
@@ -47,13 +47,13 @@ extends MantisPlugin
 					'<label for="DarkModeSwitch">Dark Mode</label>' .
 				 '</td>' .
 				 '<td>' .
-					 '<input id="DarkModeSwitch" type="checkbox" name="' . CFG_ENABLED . '" value="1" ' . ( $this->is_enabled() ? 'checked' : '' ) . '/>' .
+					 '<input id="DarkModeSwitch" type="checkbox" name="' . self::CFG_ENABLED . '" value="1" ' . ( $this->is_enabled() ? 'checked' : '' ) . '/>' .
 				 '</td>' .
 			 '</tr>';
 	}
 
 	function account_update( $p_event, $p_user_id )
 	{
-		config_set( self::CFG_ENABLED, gpc_get_bool( CFG_ENABLED, false ), $p_user_id, ALL_PROJECTS );
+		config_set( self::CFG_ENABLED, gpc_get_bool( self::CFG_ENABLED, false ), $p_user_id, ALL_PROJECTS );
 	}
 }
